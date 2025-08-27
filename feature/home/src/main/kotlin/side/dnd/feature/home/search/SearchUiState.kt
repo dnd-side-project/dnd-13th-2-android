9e6eba88774f311433e55a198b6a8948743af98f
jino <whangwan@gmail.com>
2025-08-27 15:03:36 +0900

    feat: 검색 화면 서버 API 연결 작업

--- feature/home/src/main/kotlin/side/dnd/feature/home/search/SearchUiState.kt
+++ feature/home/src/main/kotlin/side/dnd/feature/home/search/SearchUiState.kt
@@ -1,21 +1,21 @@
 package side.dnd.feature.home.search
 
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Stable
 import side.dnd.core.compositionLocals.NavigationAction
-import side.dnd.feature.home.HomeNavigationAction
 import side.dnd.feature.home.state.StoreType
 
 @Stable
 data class SearchUiState(
-    val isSearchReady: Boolean,
-    val selectedCategory: String
+    val selectedCategory: String,
+    val textFieldState: TextFieldState,
 ) {
     val pageCount: Int = 2
 
     companion object {
         val EMPTY = SearchUiState(
-            isSearchReady = false,
-            selectedCategory = ""
+            selectedCategory = "",
+            textFieldState = TextFieldState()
         )
     }
 }
@@ -23,7 +23,8 @@ data class SearchUiState(
 sealed class SearchEvent {
     data class SwitchPage(val storeType: StoreType) : SearchEvent()
     data class OnSelectChip(val selectedCategory: String) : SearchEvent()
-    data class onBrowse(val searchWord: String) : SearchEvent()
+    data object OnBrowse : SearchEvent()
+    data object OnSearch : SearchEvent()
     data object ResetSelectedCategories : SearchEvent()
     data object PopBackStack : SearchEvent()
 }
